06b41f8e8616645b58c2108e4cb47a6c74c88246
WendelSReis <wendel.m.w18@gmail.com>
2024-01-22 20:03:45 -0300

    Criando POST

--- src/main/java/my/groupId/rest/UserResource.java
+++ src/main/java/my/groupId/rest/UserResource.java
@@ -1,12 +1,19 @@
 package my.groupId.rest;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import my.groupId.rest.dto.CreateUserRequest;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
+    @POST
     public Response createUser(CreateUserRequest userRequest) {
         return Response.ok().build();
 
