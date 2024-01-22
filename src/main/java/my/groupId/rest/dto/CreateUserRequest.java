bcc84f86e32ee15e9f27d69c82058c6d17d91d3f
WendelSReis <wendel.m.w18@gmail.com>
2024-01-22 20:00:55 -0300

    Ajustes

--- src/main/java/my/groupId/rest/dto/CreateUserRequest.java
+++ src/main/java/my/groupId/rest/dto/CreateUserRequest.java
@@ -8,15 +8,12 @@ public class CreateUserRequest {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public Integer getAge() {
         return age;
     }
-
     public void setAge(Integer age) {
         this.age = age;
     }
