f02b71cc5ab6ef3a899e34a5688cb13596255119
WendelSReis <wendel.m.w18@gmail.com>
2024-02-05 21:40:10 -0300

    Entity Panache

--- src/main/java/my/groupId/domain/model/User.java
+++ src/main/java/my/groupId/domain/model/User.java
@@ -1,11 +1,13 @@
 package my.groupId.domain.model;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User extends PanacheEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/my/groupId/rest/UserResource.java
+++ src/main/java/my/groupId/rest/UserResource.java
@@ -3,6 +3,7 @@ package my.groupId.rest;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import my.groupId.domain.model.User;
 import my.groupId.rest.dto.CreateUserRequest;
 
 @Path("/users")
@@ -12,7 +13,14 @@ public class UserResource {
 
     @POST
     public Response createUser(CreateUserRequest userRequest) {
-        return Response.ok(userRequest).build();
+
+        User user = new User();
+        user.setAge(userRequest.getAge());
+        user.setName(userRequest.getName());
+
+        user.persist();
+
+        return Response.ok(user).build();
     }
 
     @GET
